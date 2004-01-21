346e24b8ed649de963a5dd7fefd5589bfc2ab201
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2004-01-20 22:53:35 -0500

    hw2_p62_1-4

---
+++ hw2_p62_1-4/Hw2.java
@@ -0,0 +1,12 @@
+mark mckelvy
+        java
+        jan 20,2004
+        hw#2
+
+        1.black,blue,cyan,darkGray,gray,green,lightGray,magenta,orange,pink,white,yellow.
+
+        2.Use a constructor that you can pass the values for red,green,and blue,and just pass equal amounts of red and blue.
+
+        3.drawRect();
+
+        4.drawString();
